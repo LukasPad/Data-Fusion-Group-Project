7c8138a22f26c6687f8d380989b8a488ae529912
LukasPad <lukas.padolevicius@gmail.com>
2023-03-27 12:32:43 +0200

    improved expert binary

--- data/DataLoading.py
+++ data/DataLoading.py
@@ -21,7 +21,23 @@ def get_df(filepath :str = "seedling_labels.csv"):
     df["average_expert"] = (df["Expert 1"] + df["Expert 2"]  + df["Expert 3"] + df["Expert 4"]) / 4
 
     #Transform this into the binray of the task (good or poor)
-    df["expert_binary"] = np.where(df["average_expert"] <= 2, 1, 0)
+    exp_binary = []
+    for x, y in df[["Expert 1", "Expert 2", "Expert 3", "Expert 4"]].iterrows():
+        healthy = 0
+        unhealthy = 0
+        for z in y:
+            if z > 2:
+                unhealthy += 1
+            else:
+                healthy += 1
+        if healthy > 2:
+            exp_binary.append(1)
+        elif unhealthy > 2:
+            exp_binary.append(0)
+        else:
+            exp_binary.append(np.NaN)
+    df["binary_expert"] = exp_binary
+    df = df.dropna()
     return df
 
 
